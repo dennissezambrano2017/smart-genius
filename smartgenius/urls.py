f7602f1101c79757d05401e14f71c276e729cac9
dennissezambrano2017 <66701370+dennissezambrano2017@users.noreply.github.com>
2023-09-02 17:40:57 -0500

    COMPLET 2.0

--- smartgenius/urls.py
+++ smartgenius/urls.py
@@ -82,66 +82,14 @@ urlpatterns = [
     path('visualizar_puntuacion_filtrada/', views.visualizar_puntuacion_filtrada, name='visualizar_puntuacion_filtrada'),
     path('visualizar_ejercicio_user/', views.obtener_ejercicios_visualizacion, name='visualizar_ejercicio_user'),
 
-    #file, name='perfil'),
-    path('logout/', views.signout, name='logout'),
-    path('signin/', views.signin, name='signin'),
-    path('inicio/', views.inicio, name='inicio'),
+    #perfil
     path('cambiar_contrasena/', views.change_password, name='cambiar_contrasena'),
     path('get_user_data/', views.get_user_data, name='get_user_data'),
     path('edit_perfil/', views.edit_usuario, name='edit_perfil'),
 
-    # Sección unidad
-    path('unidad/', views.vwUnidad, name='unidad'),
-    path('create_unidad/', views.create_unidad, name='create_unidad'),
-    path('edit_unidad/', views.edit_unidad, name='editar_unidad'),
-    path('obtener_unidad/', views.vwGetUnidad, name='obtener_unidad'),
-    path('buscar_unidad/', views.vwBuscarUnidad, name='buscar_unidad'),
-    path('eliminar_unidad/', views.vwEliminarUnidad, name='eliminar_unidad'),
-
-    # Sección contenido
-    path('contenido/', views.vwcontenido, name='contenido'),
-    path('obtener_unidades/', views.vwObtener_Unidad, name='unidad_contenido'),
-    path('create_contenido/', views.create_contenido, name='create_contenido'),
-    path('unidad_seleccionada/', views.vwGetUnidad_Contenido, name='unidad_seleccionada'),
-    path('edit_contenido/', views.vwEditar_Contenido, name='editar_contenido'),
-    path('eliminar_contenido/', views.vwEliminarContenido, name='eliminar_contenido'),
-
-    # Sección contenido
-    path('temas/', views.vwTemas, name='temas'),
-    path('obtener_contenido/', views.vwObtener_Contenido, name='contenido_tema'),
-    path('create_tema/', views.vwCreate_tema, name='create_tema'),
-    path('contenido_seleccionada/', views.vwGetContenido_Tema, name='contenido_seleccionada'),
-    path('edit_tema/', views.vwEditar_Tema, name='editar_tema'),
-    path('eliminar_tema/', views.vwEliminarTema, name='eliminar_tema'),
-
-    # Sección material
-    path('material/', views.vwMaterial, name='material'),
-    path('material_seleccionada/', views.vwGetMaterial_Tema, name='material_seleccionada'),
-    path('ejercicio_material/',views.vwGetMaterial_ejercicio, name='ejercicio_material'),
-    path('obtener_temas/', views.vwObtener_Temas, name='obtener_temas'),
-    path('create_material/', views.vwCreate_material, name='create_material'),
-    path('eliminar_material/', views.vwEliminarMaterial, name='eliminar_material'),
-    path('edit_material/', views.vwEdit_Material, name='edit_material'),
-
-    # Sección ejercicio
-    path('create_ejercicio/', views.vwCreate_ejercicio, name='create_ejercicio'),
-    path('eliminar_ejercicio/', views.vwEliminarEjercicio, name='eliminar_ejercicio'),
-
-     # Sección contenido - Alumno
-    path('contenido_alumno/', views.vwContenidoAlumno, name='contenido_alu'),
-    path('getcontenido_alumno/', views.get_material_by_tema_id,name='getcontenido_alumno'),
-    path('avance_alumno/', views.visualizar_puntuacion, name='avance_alu'),
-    path('obtener_contenidos/', views.obtener_contenidos, name='obtener_contenidos'),
-    path('obtener_temas_contenido/', views.obtener_temas_contenido, name='obtener_temas_contenido'),
-    path('obtener_ejercicios/', views.obtener_ejercicios, name='obtener_ejercicios'),
-    path('obtener_materiales/', views.obtener_materiales, name='obtener_materiales'),
-    path('visualizar_puntuacion_filtrada/', views.visualizar_puntuacion_filtrada, name='visualizar_puntuacion_filtrada'),
-    path('visualizar_ejercicio_user/', views.obtener_ejercicios_visualizacion, name='visualizar_ejercicio_user'),
-
     #Sección perfil - Alumno
     path('contenido_alumno/temas/', views.vwTemaAlumno, name='temas_alu'),
     path('perfil_alumno/', views.vwPerfilAlumno, name='perfil_alu'),
-    path('avance_alumno/', views.vwAvanceAlumno, name="avance_alu"),
     path('obtener_puntuacion_por_tema/', views.obtener_puntuacion_por_tema, name='obtener_puntuacion_por_tema'),
     path('registrar_practica/', views.RegistrarPractica, name='registrar_practica'),
 
